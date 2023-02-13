7a1b5ae04b2b9bc5b05fb14256fac90acecf5157
BatchatPro <97102064+BatchatPro@users.noreply.github.com>
2023-02-13 19:38:49 +0500

    test project added

---
+++ EDO.ProApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EDO.ProApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ EDO.ProApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EDO.ProApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
