486a1b10184fde6828a182784236e424cf238f14
BatchatPro <97102064+BatchatPro@users.noreply.github.com>
2023-02-13 18:09:03 +0500

    DEVEXPRESS ADDED

--- EDO.WorkFlow/SupportedLanguage.cs
+++ EDO.WorkFlow/SupportedLanguage.cs
@@ -1,21 +1,22 @@
-﻿namespace Frontend
+﻿using System.Collections.Generic;
+
+namespace EDO.WorkFlow;
+
+public class SupportedLanguage
 {
-    public class SupportedLanguage
+    public readonly string Caption;
+
+    public readonly string LangCode;
+
+    public SupportedLanguage(string caption, string langCode)
     {
-        public readonly string Caption;
-
-        public readonly string LangCode;
-
-        public SupportedLanguage(string caption, string langCode)
-        {
-            Caption = caption;
-            LangCode = langCode;
-        }
-
-        public static readonly IReadOnlyList<SupportedLanguage> List = new[] {
-            new SupportedLanguage("Русский", "ru"),
-            new SupportedLanguage("English", "en"),
-            new SupportedLanguage("O'zbekcha", "uz"),
-        };
+        Caption = caption;
+        LangCode = langCode;
     }
+
+    public static readonly IReadOnlyList<SupportedLanguage> List = new[] {
+        new SupportedLanguage("Русский", "ru"),
+        new SupportedLanguage("English", "en"),
+        new SupportedLanguage("O'zbekcha", "uz"),
+    };
 }
