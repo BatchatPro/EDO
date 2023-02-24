1b659a58d899fec183e1c83b5be5e818c7cea151
BatchatPro <97102064+BatchatPro@users.noreply.github.com>
2023-02-24 18:50:32 +0500

    Swagger Authorrized

--- EDO.API/Program.cs
+++ EDO.API/Program.cs
@@ -4,6 +4,7 @@ using EDO.API;
 using EDO.Database;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,16 +34,42 @@ builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(option =>
 .AddEntityFrameworkStores<AccessDbContext>()
 .AddDefaultTokenProviders()
 .AddClaimsPrincipalFactory<MyUserClaimsPrincipalFactory>();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 await app.UseRoleInitializerMiddleware();
 
