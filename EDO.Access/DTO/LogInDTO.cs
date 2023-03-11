2f406f0c8aed740e37b96f87e4deb19bca84e0c6
Abdulaziz <97102064+BatchatPro@users.noreply.github.com>
2023-03-11 14:51:31 +0500

    Required feilds added

--- EDO.Access/DTO/LogInDTO.cs
+++ EDO.Access/DTO/LogInDTO.cs
@@ -1,7 +1,11 @@
-﻿namespace EDO.Access.DTO;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EDO.Access.DTO;
 
 public class LogInDTO
 {
+    [Required(ErrorMessage = "This Poly is Required.")]
     public string UserName { get; set; }
+    [Required(ErrorMessage = "This Poly is Required.")]
     public string Password { get; set; }
 }
--- EDO.Access/DTO/RegistrationDTO.cs
+++ EDO.Access/DTO/RegistrationDTO.cs
@@ -5,19 +5,23 @@ namespace EDO.Access.DTO;
 
 public class RegistrationDTO
 {
-    [Required]
+    [Required(ErrorMessage = "This Poly is Required.")]
     public string UserName { get; set; }
-    [Required]
+    [Required(ErrorMessage = "This Poly is Required.")]
     [StringLength(100, ErrorMessage = "Minimum Length = 6 !", MinimumLength = 6)]
     [DataType(DataType.Password)]
     public string Password { get; set; }
+    [Required(ErrorMessage = "This Poly is Required.")]
     [DataType(DataType.Password)]
     [Display(Name = "Confirm password")]
     [Compare("Password", ErrorMessage = "It is not the same Password!")]
     public string ConfirmPassword { get; set; }
+    [Required(ErrorMessage = "This Poly is Required.")]
     public string LastName { get; set; }
+    [Required(ErrorMessage = "This Poly is Required.")]
     public string FirstName { get; set; }
     [AllowNull]
     public string? Email { get; set; }
+    [Required(ErrorMessage = "This Poly is Required.")]
     public string? PhoneNumber { get; set; }
 }
